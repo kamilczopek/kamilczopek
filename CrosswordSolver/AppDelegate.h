9cfd0f69de09f2fd91a0ff85cf460da0b1afe3c1
Kamil Czopek <kamil.czopek@mobica.com>
2014-11-30 12:08:02 +0100

    Initial Commit

---
+++ CrosswordSolver/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CrosswordSolver
+//
+//  Created by Kamil Czopek on 30.11.2014.
+//  Copyright (c) 2014 Kamil Czopek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CrosswordSolver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrosswordSolver
+//
+//  Created by Kamil Czopek on 30.11.2014.
+//  Copyright (c) 2014 Kamil Czopek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
